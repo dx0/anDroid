1713129ece05efcdb4bcf0312ea2f371c8f658cf
dx0 <onishchenkos.0@gmail.com>
2015-12-17 12:24:52 +0200

    first commit

---
+++ LessonLayout/app/src/main/java/com/example/dx/lessonlayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dx.lessonlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    ListView listView = (ListView)findViewById(R.id.listView1);
+
+    String[] listElements = getResources().getStringArray(R.array.listArray);
+}
