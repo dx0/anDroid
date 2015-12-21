d8f590dce15ec124034071f80c39d8e714b74bc7
dx0 <onishchenkos.0@gmail.com>
2015-12-21 13:10:33 +0200

    second HW

---
+++ SecondHomeWork/app/src/main/java/com/example/dx/secondhomework/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.dx.secondhomework;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sergio on 20-Dec-15.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
